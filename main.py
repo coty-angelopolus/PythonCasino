fcc72f4bc4c219bb122deed0c6d4dd095a402f8d
Coty Angelopolus <98544052+coty-angelopolus@users.noreply.github.com>
2025-02-27 11:15:01 -0500

    initial trail commits

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def Casino_TUI():
+    print("*****************Casino TUI**************************")
+    print("\n\nWhat would you like to do?")
 
 
-# Press the green button in the gutter to run the script.
+def account_login():
+    print("do you have an account associated with clam betting?")
+    print("Y or N")
+    usersel1 = input("\n> ").lower()
+    if usersel1 == "y":
+        print("Enter in your username")
+        player_username = input("\n> ")
+        try:
+            with open(f"players_login.txt", "r") as file:
+                for line in file
+
+    elif usersel1 == "n":
+        print("You do not have an account")
+    else:
+        print("Please enter Y or N")
+
+
+
+
+def main():
+
+    print("Hello everyone and welcome to my casino project that uses AI to simulate other players and betters")
+    print("\nThis is a work in progress that is for training anf for fun that I will continue to build upon")
+    print("\nFirst commit is on 2/26/25. Created by Coty Angelopolus using Gemini api")
+
+    input("\nPress enter to login or create an account")
+
+    account_login()
+
+
+    Casino_TUI()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm hello')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
